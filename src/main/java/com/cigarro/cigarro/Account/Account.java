0a766c84dd1ba1993d17c5351710fdcfe52253ec
David Nguyen <davebox309@gmail.com>
2022-01-02 22:01:33 -0500

    added notes

--- src/main/java/com/cigarro/cigarro/Account/Account.java
+++ src/main/java/com/cigarro/cigarro/Account/Account.java
@@ -2,4 +2,11 @@ package com.cigarro.cigarro.Account;
 
 public class Account {
     
+
+    /* TODO: 
+
+     boot strap or use another framework to handle the account management in a more
+     secure way. An industry tested one. 
+
+    */ 
 }
